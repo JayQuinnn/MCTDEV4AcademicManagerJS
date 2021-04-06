ca5c18305c98409d34607d7eee89da5ac608dfab
MitchVanHove <mitch.van.hove@student.ehb.be>
2021-04-06 14:40:26 +0200

    formJS

--- AcademicManager/src/app.js
+++ AcademicManager/src/app.js
@@ -1,3 +1,43 @@
 function def(){
-    console.log("jaja, het werkt!")
+
+    const createStudent = () => {
+
+    const Name = document.getElementById("Name").value;
+    const LastName = document.getElementById("LastName").value;
+    const Course = document.getElementById("Course").value;
+    const StudentID = document.getElementById("StudentID").value;
+    const Sex = document.getElementById("Sex").value;
+    const Picture = document.getElementById("Picture").value;
+    const Email = document.getElementById("Email").value;
+    const Disabilities = document.getElementById("Disablities").value;
+    const PhoneNumber = document.getElementById("PhoneNumber").value;
+    const Year = document.getElementById("Year").value;
+    const Grades = document.getElementById("Grades").value;
+    const Group = document.getElementById("Group").value;
+
+    const Student = {
+        Name,
+        LastName,
+        Course,
+        StudentID,
+        Sex,
+        Picture,
+        Email,
+        Disabilities,
+        PhoneNumber,
+        Year,
+        Grades,
+        Group
+    }
+
+    console.log(Student);
+
+    }
+
+
+
+document.getElementById("FormButton").addEventListener('click', () => {
+    console.log('clicked');
+    createStudent();
+});
 }
