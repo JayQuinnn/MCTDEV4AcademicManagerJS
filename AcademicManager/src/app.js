84a04cfded0196afb998214ea122f6699a64e46d
MitchVanHove <mitch.van.hove@student.ehb.be>
2021-04-07 11:32:43 +0200

    DB connect

---
+++ AcademicManager/src/app.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    host: '94.224.211.168:25568',
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+});
+
+
+function test(){
+connection.connect();
+connection.query(`INSERT INTO tblStudent(fldName,fldLastName,fldCourse,fldGender,fldPicture,fldEmail, fldDisabilities, fldPhoneNumber, fldYear, fldGroup, fldAddress) VALUES('test','test','test','MALE','test','test','test','test',1,'MCT','test')`);
+console.log('success');
+connection.end();
+/**connection.query({
+    sql: 'SELECT * FROM `books` WHERE `author` = ?',
+    timeout: 40000, // 40s
+    values: ['David']
+  }, function (error, results, fields) **/
+}
+
+test();
+console.log('running');
