653772e11dd48efbdb0cd1616745dd28e2f3b0ab
MitchVanHove <mitch.van.hove@student.ehb.be>
2021-04-10 12:45:23 +0200

    updateStudent dynamic

--- AcademicManager/src/sql.js
+++ AcademicManager/src/sql.js
@@ -7,8 +7,24 @@ let connection = mysql.createConnection({
     database: 'academic02'
 });
 
+class student {
+    constructor(Name, LastName, CourseID, Sex, Picture, Email, Disabilities, PhoneNumber, Year, Group, Address){
+        this.Name = Name;
+        this.LastName = LastName;
+        this.CourseID = CourseID;
+        this.Sex = Sex;
+        this.Picture = Picture;
+        this.Email = Email;
+        this.Disabilities = Disabilities;
+        this.PhoneNumber = PhoneNumber;
+        this.Year = Year;
+        this.Group = Group;
+        this.Address = Address;
+    }
+}
+
 const DefaultStudent = {
-    Name: "Het",
+    Name: "Mitch",
     LastName: "Test Object",
     CourseID: 1,
     Sex: "MALE",
@@ -36,7 +52,7 @@ function searchOn(tbl,fld,value){
 
 function addStudent(student){
     connection.connect();
-    connection.query(`INSERT INTO tblStudent(fldName,fldLastName,fldCourse,fldGender,fldPicture,fldEmail, fldDisabilities, fldPhoneNumber, fldYear, fldGroup, fldAddress) VALUES('${student.Name}','${student.LastName}','${student.Course}','${student.Sex}','${student.Picture}','${student.Email}','${student.Disabilities}','${student.PhoneNumber}',${student.Year},'${student.Group}','${student.Address}')`);
+    connection.query(`INSERT INTO tblStudent(fldName,fldLastName,fldCourseID,fldGender,fldPicture,fldEmail, fldDisabilities, fldPhoneNumber, fldYear, fldGroup, fldAddress) VALUES('${student.Name}','${student.LastName}','${student.CourseID}','${student.Sex}','${student.Picture}','${student.Email}','${student.Disabilities}','${student.PhoneNumber}',${student.Year},'${student.Group}','${student.Address}')`);
     connection.end();
 }
 
@@ -48,10 +64,12 @@ function getAllStudents(){
     connection.end();
 }
 
-function removeStudent(firstName,lastName){
-  searchStudents()
+function removeStudent(studentID){
   connection.connect();
-  connection.query('DELETE FROM tblStudent WHERE fldstudentid = 1', function (error, results, fields) {
+
+  console.log(`Removing student with ID: ${studentID}`);
+
+  connection.query(`DELETE FROM tblStudent WHERE fldstudentid = ${studentID}`, function (error, results, fields) {
       if (error) throw error;
       console.log('deleted ' + results.affectedRows + ' rows');
     })
@@ -71,18 +89,27 @@ function searchStudents(fld, value){
 }
 
 function updateStudent(studentid, student){
-    console.log("searching for _____");
     connection.connect();
-    connection.query(`UPDATE tblStudent SET fldName='Quinnzay', WHERE fldstudentid=1`)
+
+    console.log(`Updating ${student.Name} ${student.LastName}`);
+
+    connection.query(`UPDATE tblStudent SET fldName='${student.Name}' , fldLastName='${student.LastName}', fldCourseID='${student.CourseID}', fldGender='${student.Sex}', fldPicture='${student.Picture}', fldEmail='${student.Email}', fldDisabilities='${student.Disabilities}', fldPhoneNumber='${student.PhoneNumber}'   WHERE fldstudentid=${studentid}`)
     connection.end();
 }
 
-//addStudent(DefaultStudent);
-//getAllStudents();
-//removeStudent();
-//searchStudents("fldCourse","MCT");
+let jochem = new student('Jochemmmm', 'Crab', 1, 'MALE', 'picture', 'jochem@jochem', 'Geen', '011111111111', '2', 'C', 'Leuven')
+let mitch = new student('Mitch', 'Van Hove', 1, 'MALE', 'picture2', 'jochem@jochem', 'Geen', '011456461111', '3', 'B', 'Dilbeek')
 
-updateStudent();
+
+//getAllStudents();
+//removeStudent(2);
+//updateStudent(1, jochem);
+//addStudent(mitch);
+//searchStudents("fldCourseID","1");
+
+
+
+
 
 
 
