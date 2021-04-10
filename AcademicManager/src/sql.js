3f7dadb3f4c338992db4b231905760a61200ef23
MitchVanHove <mitch.van.hove@student.ehb.be>
2021-04-10 15:33:54 +0200

    update Exchange

--- AcademicManager/src/sql.js
+++ AcademicManager/src/sql.js
@@ -141,24 +141,7 @@ function getAllExchange(){
 }
 
 function renderAllAlmas(){
-    let allAlma = document.getElementById('allAlma');
-    let htmlString = ``;
-    connection.connect();
-    connection.query(`SELECT * FROM tblalma`,function(error, results, field){
-        console.log(results);
-        results.forEach(element => {
-            htmlString = htmlString + `
-            <div class="row">
-                <div class="col">${element.fldName}</div>
-                <div class="col">${element.fldAddress}</div>
-                <div class="col">${element.fldNotes}</div>
-                <div class="col">${element.fldEmail}</div>
-                <div class="col">${element.fldPhoneNumber}</div>
-            </div>`;
-            allAlma.innerHTML = htmlString;
-        });
-    })
-    connection.end();
+
 }
 
 
@@ -198,7 +181,8 @@ function updateExchange(studentid, student){
 
     console.log(`Updating ${student.Name} ${student.LastName}`);
 
-    connection.query(`UPDATE tblstudentexchange SET fldName='${student.Name}' , fldLastName='${student.LastName}', fldGender='${student.Sex}', fldPicture='${'1213154'}' `)
+    connection.query(`UPDATE tblstudentexchange SET fldName='${student.Name}' , fldLastName='${student.LastName}', fldGender='${student.Sex}', fldPicture='${student.Picture}', fldEmail='${student.Email}', fldDisabilities='${student.Disabilities}, fldPhoneNumber='${student.PhoneNumber}', fldYear='${student.Year}', fldGroup='${student.Group}', fldNationality='${student.Nationality}', fldAlmaID='${student.Alma}', fldDormID='${student.Dorm}', fldMotivationalLetter='${student.Letter}'`);
+    connection.end();
 }
 
 
